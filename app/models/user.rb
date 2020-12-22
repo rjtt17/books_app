97c4ea5882e6934190ebb0cf3d812e34938e7a3c
rjtt17 <rjtt17@gmail.com>
2020-12-22 09:24:50 +0900

    [update]rubocopでのエラーを解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name   # assuming the user model has a name
-      user.avatar.attach(io:URI.parse(auth.info.image).open, filename: "#{user.name}_image.png" )
+      user.avatar.attach(io: URI.parse(auth.info.image).open, filename: "#{user.name}_image.png")
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
